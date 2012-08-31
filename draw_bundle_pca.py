07298deed13a8e9434fd97e498b48bb27405eeb0
Alex Flint <alex.flint@gmail.com>
2012-08-30 23:13:45 -0400

    more sliding window slam stuff

--- draw_bundle_pca.py
+++ draw_bundle_pca.py
@@ -8,17 +8,33 @@ def plot_points(xs, *args, **kwargs):
     xs = asarray(xs)
     plt.plot(xs[:,0], xs[:,1], *args, **kwargs)
 
-def draw_pose(R, t, subspace, color):
+def draw_pose(R, t, subspace=None, color=None):
+    if subspace is None:
+        subspace = eye(3)[:2]
+    if color is None:
+        color = 'r'
+
     C = -dot(R.T, t)
     Cp = pca.project(C, subspace)
     for i in range(3):
         ep = pca.project(C + R[:,i], subspace)
         plot_points([ Cp, ep ], '-'+color)
 
-def draw_point_cloud(xs, subspace):
+def draw_poses(Rs, ts, subspace=None, color=None):
+    plt.clf()
+    plt.xlim(-10,10)
+    plt.ylim(-10,10)
+    for R,t in zip(Rs,ts):
+        draw_pose(R,t,subspace,color)
+
+def draw_point_cloud(xs, subspace=None):
+    if subspace is None:
+        subspace = eye(3)[:2]
     plot_points(pca.project(xs, subspace), '.b')
 
-def draw_bundle(bundle, subspace, pose_colors=None):
+def draw_bundle(bundle, subspace=None, pose_colors=None):
+    if subspace is None:
+        subspace = eye(3)[:2]
     #mpl.rcParams['legend.fontsize'] = 10
 
     plt.figure()
@@ -42,4 +58,3 @@ if __name__ == '__main__':
         track.reprojection = bundle.triangulate(track)
 
     draw_bundle(bundle)
-
--- window_slam.py
+++ window_slam.py
@@ -1,26 +1,17 @@
 import sys
 import numpy as np
 from numpy import *
-
-from bundle_adjuster import BundleAdjuster
-import bundle
-import bundle_io
-import sequence
+from copy import deepcopy
+import geometry
 
 import matplotlib
 matplotlib.use('PDF')
 import matplotlib.pyplot as plt
 
-from draw_bundle_pca import draw_bundle, compute_subspace
-
-class SlidingWindowSLAM(object):
-    def __init__(self, window_size):
-        self.window_size = window_size
-        
-    def run(self, measurements, init_poses):
-        nf = len(sequence.Rs)
-        for t in range(nf-self.window_size+1):
-            b = sequence.get_window(t, self.window_size)
+from draw_bundle_pca import draw_bundle, compute_subspace, draw_pose
+from bundle_adjuster import BundleAdjuster
+import bundle
+import bundle_io
 
 
 def run(complete_bundle, window_size, num_to_freeze=2, pdf_pattern=None):
@@ -39,12 +30,22 @@ def run(complete_bundle, window_size, num_to_freeze=2, pdf_pattern=None):
     for i in range(0, len(complete_bundle.cameras)-window_size+1):
         print '\n\n==============\nWINDOW: [%d..%d]\n' % (i, i+window_size)
 
+        prev_bundle = deepcopy(cur_bundle)
+
         # Adjust this window
         camera_ids = range(i, i+window_size)
         ba = BundleAdjuster()
         ba.set_bundle(cur_bundle, camera_ids=camera_ids, track_ids=track_ids)
         ba.optimize()
         cur_bundle = ba.bundle
+
+        # Propagate the update to the next camera
+        next_camera_id = i + window_size
+        if next_camera_id < len(cur_bundle.cameras):
+            geometry.propagate_pose_update_inplace(prev_bundle.cameras[i],
+                                                   cur_bundle.cameras[i],
+                                                   cur_bundle.cameras[i])
+        
         
         # Draw this window
         print 'Drawing bundle...'
@@ -55,11 +56,15 @@ def run(complete_bundle, window_size, num_to_freeze=2, pdf_pattern=None):
             for j in range(i,i+window_size):
                 pose_colors[j] = 'r'
             pdf_path = pdf_pattern % i
+
             print 'Writing to ',pdf_path
             draw_bundle(ba.bundle, visualization_subspace, pose_colors)
+            if next_camera_id < len(prev_bundle.cameras):
+                draw_pose(prev_bundle.cameras[next_camera_id].R,
+                          prev_bundle.cameras[next_camera_id].t,
+                          visualization_subspace,
+                          'c')
             plt.savefig(pdf_path)
-
-        break
 
 if __name__ == '__main__':
     np.seterr(all='raise')
